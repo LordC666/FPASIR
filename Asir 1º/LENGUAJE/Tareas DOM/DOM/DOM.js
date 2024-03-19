166a2b14be9a20479403cca30e28f25671cad4cf
LordC666 <148909056+LordC666@users.noreply.github.com>
2024-03-19 11:18:55 +0100

    Delete Asir 1º directory

--- Asir 1º/LENGUAJE/Tareas DOM/DOM/DOM.js
+++
@@ -1,9 +0,0 @@
-function colores(){
-    var titutlo = document.getElementById("cambio");
-    titutlo.style.color = "red";
-}
-
-function tamaño(){
-    var titulo = document.getElementById("cambio");
-    titulo.style.fontSize = "50px";
-}
--- Asir 1º/LENGUAJE/Tareas DOM/DOM/js/DOM.js
+++
@@ -1,9 +0,0 @@
-function cambiarColor(){
-    var titutlo = document.getElementById("adaptable");
-    titutlo.style.color = "red";
-}
-
-function cambiarTamanio(){
-    var titulo = document.getElementById("adaptable");
-    titulo.style.fontSize = "50px";
-}
