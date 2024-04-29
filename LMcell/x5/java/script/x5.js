d39cfc1ffbed2166771f3db43ed686e72767a3b7
César Nieto Silva <148909056+LordC666@users.noreply.github.com>
2024-04-29 22:28:31 +0200

    Update x5.js

--- LMcell/x5/java/script/x5.js
+++ LMcell/x5/java/script/x5.js
@@ -5,7 +5,7 @@ function multiplication() {
     let i;
 
     for (i = 0; i <= 50; i+=5){
-        tab += i + "<br>";
+        tab += i + ", ";
     }
 
     document.getElementById('result').innerHTML = "La tabla del 5 es... " + tab;
